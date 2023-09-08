73f396e6f6eb82e67cd7a8c60b297c206b5feecd
Knifer <yeded73422@invodua.com>
2023-09-08 12:46:31 +0300

    added display of characters on the movie page

--- src/moduls/Root/AllFilmsPage/FilmPage/FilmPageComponents/CharacterComponent.tsx
+++ src/moduls/Root/AllFilmsPage/FilmPage/FilmPageComponents/CharacterComponent.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query";
 import { filmDataInterface } from "../../AllFilmsPageInterface";
-import { FetchSwApiPeople } from "../../../../../Fetch/FetchSwapiData";
+import { FetchSwApiPeopleById } from "../../../../../Fetch/FetchSwapiData";
 import "./CharacterComponent.scss";
 
 const CharacterComponent: React.FC<{ character: string }> = ({ character }) => {
@@ -12,7 +12,7 @@ const CharacterComponent: React.FC<{ character: string }> = ({ character }) => {
   }
   const { data, isLoading, isError, error } = useQuery<filmDataInterface>(
     `people${character}`,
-    () => FetchSwApiPeople(character),
+    () => FetchSwApiPeopleById(id),
     {
       refetchOnWindowFocus: false,
     }
@@ -27,36 +27,34 @@ const CharacterComponent: React.FC<{ character: string }> = ({ character }) => {
   }
 
   return (
-    <div className="character">
-      <div className="character-info">
-        <img
-          className="character-img"
-          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-          alt=""
-        ></img>
-        <h2 className="character-name">{data.name}</h2>
-        <p>
-          <strong>Height:</strong> {data.height} cm
-        </p>
-        <p>
-          <strong>Mass:</strong> {data.mass} kg
-        </p>
-        <p>
-          <strong>Hair Color:</strong> {data.hair_color}
-        </p>
-        <p>
-          <strong>Skin Color:</strong> {data.skin_color}
-        </p>
-        <p>
-          <strong>Eye Color:</strong> {data.eye_color}
-        </p>
-        <p>
-          <strong>Birth Year:</strong> {data.birth_year}
-        </p>
-        <p>
-          <strong>Gender:</strong> {data.gender}
-        </p>
-      </div>
+    <div className="character-info">
+      <img
+        className="character-img"
+        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+        alt="character img"
+      ></img>
+      <h2 className="character-name">{data.name}</h2>
+      <p>
+        <strong>Height:</strong> {data.height} cm
+      </p>
+      <p>
+        <strong>Mass:</strong> {data.mass} kg
+      </p>
+      <p>
+        <strong>Hair Color:</strong> {data.hair_color}
+      </p>
+      <p>
+        <strong>Skin Color:</strong> {data.skin_color}
+      </p>
+      <p>
+        <strong>Eye Color:</strong> {data.eye_color}
+      </p>
+      <p>
+        <strong>Birth Year:</strong> {data.birth_year}
+      </p>
+      <p>
+        <strong>Gender:</strong> {data.gender}
+      </p>
     </div>
   );
 };
